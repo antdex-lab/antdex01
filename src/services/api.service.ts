0b1240f6b709b54a0b908da6e5faa21f9db4a4ec
Dhaval Bhadiyadara <rutvikanaghan12@gmail.com>
2024-09-29 11:45:07 +0530

    Services

---
+++ src/services/api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'https://dex-avn-be.onrender.com/api'; // Base URL of your API
+
+  constructor(private http: HttpClient) {}
+
+  // Generic GET method
+  getData(endpoint: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${endpoint}`);
+  }
+
+  // Generic POST method
+  createData(endpoint: string, data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/${endpoint}`, data);
+  }
+
+  // Generic PUT method
+  updateData(endpoint: string, id: string, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${endpoint}/${id}`, data);
+  }
+
+  // Generic DELETE method
+  deleteData(endpoint: string, id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${endpoint}/${id}`);
+  }
+}
